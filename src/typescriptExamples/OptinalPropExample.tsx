0a081ae4fbece406f3fdb6963482c172d15413eb
lucas <lucasportellaagu@gmail.com>
2022-04-11 17:52:15 -0300

    feat: default value for optional prop

--- src/typescriptExamples/OptinalPropExample.tsx
+++ src/typescriptExamples/OptinalPropExample.tsx
@@ -3,13 +3,15 @@ import React from "react";
 interface ComponentProps {
     someProps: string;
     optionalProp?: string;
+    anotherOptionalProp?: string;
     // the "?" symbol makes the prop optional, the component OptionalPropExample is passed in the App component only with the someProps prop, but because the optionalProp is optional, typescript doesn't raise an error
 }
 
-const OptionalPropExample = ({someProps, optionalProp}: ComponentProps) => {
+const OptionalPropExample = ({someProps, optionalProp, anotherOptionalProp='some default value'}: ComponentProps) => {
  return <section>
      <div>{someProps}</div>
      <div>{optionalProp}</div>
+     <div>{anotherOptionalProp}</div> {/* it will render 'some default value' if no prop value is passed*/}
  </section>
 }
 
